188f7a6e0c26c815b1378548ae06cd189e8bd96c
Andre Green <acgreen1226@gmail.com>
2015-08-27 01:58:08 -0700

    more progress with notifications

---
+++ Skylock/Skylock/SLAccelerometerValues.h
@@ -0,0 +1,13 @@
+//
+//  SLAccelerometerValues.h
+//  Skylock
+//
+//  Created by Andre Green on 8/26/15.
+//  Copyright (c) 2015 Andre Green. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLAccelerometerValues : NSObject
+
+@end
---
+++ Skylock/Skylock/SLAccelerometerValues.m
@@ -0,0 +1,13 @@
+//
+//  SLAccelerometerValues.m
+//  Skylock
+//
+//  Created by Andre Green on 8/26/15.
+//  Copyright (c) 2015 Andre Green. All rights reserved.
+//
+
+#import "SLAccelerometerValues.h"
+
+@implementation SLAccelerometerValues
+
+@end
