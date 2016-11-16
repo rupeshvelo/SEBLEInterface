8663438fdc9973bb81c5793a1fc213e5ef67de44
greenac <andre1226.dev@gmail.com>
2016-11-16 15:40:59 -0800

    ellipse release 1.0.3.5

---
+++ Skylock/Skylock/SLLock+CoreDataClass.h
@@ -0,0 +1,22 @@
+//
+//  SLLock+CoreDataClass.h
+//  Ellipse
+//
+//  Created by Andre Green on 11/27/16.
+//  Copyright © 2016 Andre Green. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class SLDbLockSharedContact, SLUser;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLLock : NSManagedObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#import "SLLock+CoreDataProperties.h"
---
+++ Skylock/Skylock/SLLock+CoreDataClass.m
@@ -0,0 +1,14 @@
+//
+//  SLLock+CoreDataClass.m
+//  Ellipse
+//
+//  Created by Andre Green on 11/27/16.
+//  Copyright © 2016 Andre Green. All rights reserved.
+//
+
+#import "SLLock+CoreDataClass.h"
+#import "SLDbLockSharedContact.h"
+#import "SLUser.h"
+@implementation SLLock
+
+@end
